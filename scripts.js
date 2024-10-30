486ff978fed73041f8162ea6195231b2aade6c07
iwassomethingamazing <lorisbaj10@gmail.com>
2024-10-30 05:51:57 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const availableDates = ['2023-11-01', '2023-11-03', '2023-11-05']; // Example available dates
-    const availableDatesText = document.getElementById('available-dates');
-    
-    // Display available dates
-    availableDatesText.textContent = availableDates.join(', ');
-});
-
 function changeImage(thumbnail) {
     const mainImage = document.getElementById('main-image');
     mainImage.src = thumbnail.src; // Change main image to the clicked thumbnail
